ca5323d41e6e84427cfde8ae83b20871dd331270
prakharn111 <prakharnagore111@gmail.com>
2021-12-12 20:38:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
+function App() {
+  const [index, setIndex] = useState(0);
+  const [people] = useState(data);
+
+  const nextSlide = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      if (newIndex > people.length - 1) {
+        newIndex = 0;
+      }
+      return newIndex;
+    });
+  };
+  const prevSlide = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      if (newIndex > people.length - 1) {
+        newIndex = 0;
+      }
+      return newIndex;
+    });
+  };
+
+  useEffect(() => {
+    let slider = setTimeout(() => {
+      setIndex((index) => {
+        let newIndex = index + 1;
+        if (newIndex > people.length - 1) {
+          newIndex = 0;
+        }
+        return newIndex;
+      });
+    }, 5000);
+    return () => {
+      clearTimeout(slider);
+    };
+  });
+
+  const { id, name, image, title, quote } = people[index];
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>Reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {
+          <article key={id}>
+            <img src={image} alt={name} className="person-img" />
+            <h4>{name}</h4>
+            <p className="title">{title}</p>
+            <p className="text">{quote}</p>
+            <FaQuoteRight className="icon" />
+          </article>
+        }
+        <button className="prev" onClick={prevSlide}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={nextSlide}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export default App;
